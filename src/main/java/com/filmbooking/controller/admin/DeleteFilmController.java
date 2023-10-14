161748d95a656623f4dcc65d5a92439b3f6468b9
Nguyen Phuong Nha <103343358+nphuonha2101@users.noreply.github.com>
2023-10-14 21:19:14 +0700

    - delete film img on server when delete film

--- src/main/java/com/filmbooking/controller/admin/DeleteFilmController.java
+++ src/main/java/com/filmbooking/controller/admin/DeleteFilmController.java
@@ -1,14 +1,18 @@
 package com.filmbooking.controller.admin;
 
 
+import com.filmbooking.model.Film;
 import com.filmbooking.services.impls.FilmServicesImpl;
 import com.filmbooking.services.IFilmServices;
+import com.filmbooking.utils.ContextPathUtils;
+import com.filmbooking.utils.fileUtils.FileUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet(name = "deleteFilm", value = "/delete-film")
@@ -24,7 +28,15 @@ public class DeleteFilmController extends HttpServlet {
 
         System.out.println(filmServices.getByFilmID(filmID));
 
-        filmServices.delete(filmServices.getByFilmID(filmID));
+        Film deletedFilm = filmServices.getByFilmID(filmID);
+        filmServices.delete(deletedFilm);
+
+        // delete film image
+        String filmImgFilePath = FileUtils.getRealContextPath(req)  + "src/main/webapp" + deletedFilm.getImgPath();
+        System.out.println("DeleteFilmController Test: " + filmImgFilePath);
+        File file = new File(filmImgFilePath);
+        file.delete();
+
 //
         resp.sendRedirect("admin");
 
